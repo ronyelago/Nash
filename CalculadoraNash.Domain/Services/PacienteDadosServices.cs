f5a8866b2a1977e9008dcc7c427ba622f7aa3bbd
Ronye do Lago <ronye.rocha@gmail.com>
2018-05-10 11:42:47 -0300

    serviços

---
+++ CalculadoraNash.Domain/Services/PacienteDadosServices.cs
@@ -0,0 +1,16 @@
+﻿using CalculadoraNash.Domain.Entities;
+using CalculadoraNash.Domain.Interfaces.Repositories;
+using CalculadoraNash.Domain.Interfaces.Services;
+
+namespace CalculadoraNash.Domain.Services
+{
+    public class PacienteDadosServices : ServiceBase<PacienteDados>, IPacienteDadosService
+    {
+        private readonly IPacienteDadosRepository _pacienteDadosRepository;
+
+        public PacienteDadosServices(IPacienteDadosRepository pacienteDadosrepository) : base(pacienteDadosrepository)
+        {
+            _pacienteDadosRepository = pacienteDadosrepository;
+        }
+    }
+}
---
+++ CalculadoraNash.Domain/Services/PacienteService.cs
@@ -0,0 +1,16 @@
+﻿using CalculadoraNash.Domain.Entities;
+using CalculadoraNash.Domain.Interfaces.Repositories;
+using CalculadoraNash.Domain.Interfaces.Services;
+
+namespace CalculadoraNash.Domain.Services
+{
+    public class PacienteService : ServiceBase<Paciente>, IPacienteService
+    {
+        private readonly IPacienteRepository _pacienteRepository;
+
+        public PacienteService(IPacienteRepository pacienteRepository) : base(pacienteRepository)
+        {
+            _pacienteRepository = pacienteRepository;
+        }
+    }
+}

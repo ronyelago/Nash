c3a22c4347c256caed8f2b09611cff4ba32385ab
Ronye do Lago <ronye.rocha@gmail.com>
2018-05-11 13:10:52 -0300

    styles

--- CalculadoraNash/App_Start/BundleConfig.cs
+++ CalculadoraNash/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace CalculadoraNash
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/style.css"));
         }
     }
 }
